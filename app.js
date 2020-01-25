89ecbf2ffdcd23e6b3bbeca221922a345573b5c9
juan <juanignaciocor92@gmail.com>
2020-01-25 10:45:07 -0300

    tweety terminado

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
 const express= require("express");
 const app= express();
-
+const nunjucks= require("nunjucks")
 const data= require("./data.js")
+const bodyParser= require("body-parser")
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.use(express.static('./public'));
+	
+
+function root(req,res,next){
+
+	console.log("BIENVENIDO")
+	next()
 
 
+}
 
-app.get("/", (req,res)=>{
-	console.log(req.headers)
-	res.send(data.tweets)
-
-
-})
-app.get("/login",(req,res)=>{
-
-
-
-
-})
-
-app.post("/login",(req,res)=>{
-
-
-
-
-
+app.get("/",root, (req,res)=>{
+	res.render("index.html",{tweets: data.list().map((tweet, i) => ({...tweet, id: i})) })
 })
 
 app.post("/tweet",(req,res)=>{
+	if (!req.body.name.trim() || !req.body.content.trim()) res.sendStatus(400);
+	console.log(req.body)
+	data.add(req.body.name,req.body.content)
+	res.redirect("/")
+})
 
+app.get("/tweet/:id",(req,res)=>{
+	
+	if (req.query.next == "1") return res.redirect("/tweet/"+((Number(req.params.id))+1))
+	var arregloIndice= [{ ...data.list()[req.params.id], id: req.params.id }]
+	res.render("index.html",{tweets: arregloIndice})
+	console.log(req.params.id)
+})
 
-
-
-
+app.post("/comment/:id",(req,res)=>{
+	if (!req.body.name.trim() || !req.body.content.trim()) res.sendStatus(400);
+	data.comment(req.params.id,req.body.name,req.body.content)
+	res.redirect("/tweet/"+req.params.id)
 })
 
 
+app.get("*",(req,res)=>{
 
+	res.sendStatus(404)
+
+})
 
 
 
--- data.js
+++ data.js
@@ -1,7 +1,32 @@
-module.exports={
+const tweets= []
+function add (name, content) {
+  tweets.unshift({ name: name, content: content, comments: []});
+}
+function list () {
+  return tweets;
+}
+
+function comment(id, name, content) {
+	tweets[id].comments.push({ name: name, content: content });
+}
 
 
-	tweets: [],
-	users: [],
+const randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+const getFakeName = function() {
+  const fakeFirsts = ['Toni', 'Guille', 'Santi', 'Facu', 'Alan', 'Pinki', 'Tincho', 'Solano', 'R2D2'];
+  const fakeLasts = ['Scanlan', 'Aszyn', 'Tralice', 'Velasco', 'Sainz', 'Palacio', 'Palacios', 'Lidueña', 'Fisicaro', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+const getFakeTweet = function() {
+  const awesome_adj = ['increible', 'emocionante', 'increible', 'gracioso', 'dulce', 'cool',  'sorprendente', 'impresionante'];
+  return "Plataforma 5 es " + randArrayEl(awesome_adj) + "! Los profesores simplemente son " + randArrayEl(awesome_adj) + ". #P5Love #codedreams";
+};
 
+module.exports = { add: add, list: list, comment: comment};
+
+for (let i = 0; i < 10; i++) {
+  
+  module.exports.add( getFakeName(), getFakeTweet() );
 }
